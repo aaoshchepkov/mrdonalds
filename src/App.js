eae802d58cdcd978fd04c75873bdff6950bb4e1f
aaoshchepkov <aa_oshchepkov@mail.ru>
2021-04-05 22:32:21 +0700

    создал Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { NavBar } from './Components/NavBar';
 
+const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+*,
+*::before,
+*::after{
+  box-sizing: inherit;
+}
+
+body {
+  margin: 0;
+  background-color: #f0f0f0;
+  font-family: Roboto, sans-serif;
+  font-size: 20px;
+  color: black;
+}
+img {
+  max-width:100%;
+  height: auto;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+h1, h2, h3, h4 {
+  margin: 0;
+  padding: 0;
+  font-family: Pacifico;
+}
+
+p {
+  margin: 0;
+  padding: 0;
+}
+
+`;
 
 function App() {
   return (
-    <div className="App">
-
-
-      поллдоhjh
-    </div>
+    <>
+      <GlobalStyle/>
+      <NavBar/>
+    </>
   );
 }
 
---
+++ src/Components/NavBar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import LogoImage from "../image/logo.svg";
+import UserImage from "../image/user.svg";
+
+const NavBarStyled = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  height: 80px;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  background-color: #299b01;
+  color: white;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const H1 = styled.h1`
+  font-size: 24px;
+  margin-left: 15px;
+`;
+const ImgLogo = styled.img`
+  width: 50px;
+`;
+
+const Login = styled.button`
+  background-color: #299b01;
+  border: none;
+  color: white;
+  font-size: 16px;
+  line-height: 19px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LoginImage = styled.img`
+  width: 32px;
+  margin-bottom: 3px;
+`;
+
+export const NavBar = () => (
+  <NavBarStyled>
+    <Logo>
+      <ImgLogo src={LogoImage} alt="Logo" />
+      <H1>MrDonald's</H1>
+    </Logo>
+    <Login>
+      <LoginImage src={UserImage} alt="Icon:User" />
+      войти
+    </Login>
+  </NavBarStyled>
+);
